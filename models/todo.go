f81505e6a8105439144830c9b5d1bdf6870cfca4
Jordan Garcia <arickho@gmail.com>
2017-04-16 12:28:33 -0400

    added json marshelling

--- models/todo.go
+++ models/todo.go
@@ -4,9 +4,9 @@ import "time"
 
 // Todo - Model
 type Todo struct {
-	Name      string
-	Completed bool
-	Due       time.Time
+	Name      string    `json:"name"`
+	Completed bool      `json:"completed"`
+	Due       time.Time `json:"due"`
 }
 
 // Todos - List of todo's
